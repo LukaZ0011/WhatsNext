c44caaf723b3ceed8f67f356591d86afa588b5be
LukaZ0011 <luka.zivoder110@gmail.com>
2025-10-21 17:39:17 +0200

    Added recurring events, rrule parsing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 from datetime import datetime
 from datetime import date
+from datetime import timedelta
+import copy
 
 class CalendarEvent:
     def __init__(self):
@@ -9,6 +11,55 @@ class CalendarEvent:
         self.description = None
         self.location = None
         self.title = None
+        self.rrule = None
+
+def expandRecurringEvent(e):
+    varFREQ = None
+    varWKST = None
+    varINTERVAL = 1
+    varBYDAY = None
+    varUNTIL = None
+    
+    #parse the rrule
+    rrule = e.rrule.split(";")
+    for part in rrule:
+        if "FREQ" in part:
+            varFREQ = part.split("=")[1].strip()
+        if "WKST" in part:
+            varWKST = part.split("=")[1].strip()
+        if "INTERVAL" in part:
+            varINTERVAL = int(part.split("=")[1].strip())
+        if "BYDAY" in part:
+            varBYDAY = part.split("=")[1].strip()
+        if "UNTIL" in part:
+            varUNTIL = parseDateTime(part.split("=")[1].strip())
+    
+    #skip dates that match the original start_time    
+    if varUNTIL <= e.start_time:
+            return [e]
+    elif varUNTIL is None:
+        varUNTIL = e.start_time
+
+    recurringEvents = []
+    
+    current_start = e.start_time
+    current_end = e.end_time
+    
+    #generate the events based on frequency / interval
+    while current_start <= varUNTIL:
+        recurrance = copy.deepcopy(e)
+        recurrance.start_time = current_start
+        recurrance.end_time = current_end
+        recurringEvents.append(recurrance)
+        if varFREQ == "WEEKLY":
+            current_start += timedelta(weeks=varINTERVAL)
+            current_end += timedelta(weeks=varINTERVAL)
+        elif varFREQ == "DAILY":
+            current_start += timedelta(days=varINTERVAL)
+            current_end += timedelta(days=varINTERVAL)
+            
+
+    return recurringEvents
 
 def getTodayEvents(eventList):
     todayList = []
@@ -21,6 +72,7 @@ def getTodayEvents(eventList):
 def printEvents(eventList):
     for event in eventList:
         print(f"Title: {event.title}")
+        print(f"RRule: {event.rrule}")
         print(f"Start Time: {event.start_time}")
         print(f"End Time: {event.end_time}")
         print(f"Location: {event.location}")
@@ -39,6 +91,8 @@ def parse_ics(file):
         if line.startswith("BEGIN:VEVENT") and not isNewEvent:
             isNewEvent = True
             event = CalendarEvent()
+        if line.startswith("RRULE") and isNewEvent:
+            event.rrule = line.split(":")[1].strip()
         if line.startswith("DTSTART;TZID") and isNewEvent:
             event.start_time = parseDateTime(line.split(":")[1].strip())
         if line.startswith("DTEND;TZID") and isNewEvent:
@@ -51,7 +105,10 @@ def parse_ics(file):
             event.title = line.split(":")[1].strip()
         if line.startswith("END:VEVENT") and isNewEvent:
             isNewEvent = False
-            eventList.append(event)
+            if event.rrule:
+                eventList.extend(expandRecurringEvent(event))
+            else:
+                eventList.append(event)
     return eventList
 
                 
